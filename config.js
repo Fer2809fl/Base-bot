529e41a1eb520e3986a02475c0bf468916aed2dc
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-17 18:24:36 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-'use strict';
-
 export default {
   botName: 'Asta',
   ownerNumber: ['521234567890'],
   prefix: ['.', '#', '!'],
-  sessionName: 'session',
-  usePairingCode: null,
+  authFolder: 'session',
+  phoneNumber: '',
 };
